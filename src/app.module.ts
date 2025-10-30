f47830cca7a0f21b711a85a3de0bc67b3a8d8911
lucas melo <lucas.melo044@gmail.com>
2025-10-30 19:53:46 -0300

    feat: config TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true,
+      logging: false,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    }),
+  ],
+})
 export class AppModule {}
