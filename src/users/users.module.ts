0cb92ac6037bbd65855e758e142579645b44cdd4
lucas melo <lucas.melo044@gmail.com>
2025-10-31 17:38:08 -0300

    feat: connect UserService to the Repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersService],
+})
 export class UsersModule {}
