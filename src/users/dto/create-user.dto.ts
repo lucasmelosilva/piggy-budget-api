3cffea810c6f8c64e9e9ca322b14743e57447553
lucas melo <lucas.melo044@gmail.com>
2025-11-01 18:02:14 -0300

    feat: add CreateUserDTO class

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,38 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDTO {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must contain at least one uppercase letter, one lowercase letter, and one number or special character.',
+  })
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  lastName?: string;
+
+  @IsOptional()
+  @IsUrl()
+  imageUrl?: string;
+}
