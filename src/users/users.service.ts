6d96099e9ecc88679a261fb846ac9cd760452c9e
lucas melo <lucas.melo044@gmail.com>
2025-11-01 18:06:56 -0300

    feat: add utility method
    - Switched from using `User` to `UserEntity` in the repository
      injection.
    - Added `create` method with password hashing and returning user without
      password
    - Added helper methods:
      - `findByEmail(emial: string)`
      - `findByUsername(username: string)`
      - `findById(id: string)`
      - `emailVerified(id: string)` to mark email as verified.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './users.entity';
 import { Repository } from 'typeorm';
+import { UserEntity } from './users.entity';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
   ) {}
+
+  async create(createUserDTO: CreateUserDTO): Promise<UserEntity> {
+    const user = {
+      ...createUserDTO,
+      password: await bcrypt.hash(createUserDTO.password, 10),
+    };
+    const result = await this.userRepository.save(user);
+    // eslint-disable-next-line
+    const { password, ...userWithoutPassword } = result;
+    return userWithoutPassword as UserEntity;
+  }
+
+  async findByEmail(email: string): Promise<UserEntity | null> {
+    const user = await this.userRepository.findOne({ where: { email } });
+    return user;
+  }
+
+  async findByUsername(username: string): Promise<UserEntity | null> {
+    const user = await this.userRepository.findOne({ where: { username } });
+    return user;
+  }
+
+  async findById(id: string): Promise<UserEntity | null> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    return user;
+  }
+
+  async emailVerified(id: string): Promise<void> {
+    await this.userRepository.update(id, { isEmailVerified: true });
+  }
 }
