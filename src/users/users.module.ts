a87b4e3b8bbe0547babec66787e11ba1f5611155
lucas melo <lucas.melo044@gmail.com>
2025-11-01 17:51:24 -0300

    refactor: rename UserEntity

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users.entity';
-
+import { UserEntity } from './users.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UsersService],
 })
 export class UsersModule {}
