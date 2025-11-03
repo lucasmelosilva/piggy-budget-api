60a68c48a1e5be722f538d9197b86c815e685e3b
lucas melo <lucas.melo044@gmail.com>
2025-11-03 10:05:17 -0300

    feat: implement local authentication with Passport Local strategy
    - Add AuthController with /signin route using LocalAuthGuard.
    - Integrate AuthService with UserService and bcrypt password validation.
    - Register LocalStrategy and UserModule in AuthModule.
    - Enable user validation via email and password.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new Error('Email address or password provided is incorrect.');
+  }
+}
---
+++ src/auth/guards/local-auth.guard.ts
@@ -0,0 +1,22 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user) {
+    if (err || !user) {
+      // eslint-disable-next-line
+      throw new UnauthorizedException(err?.message);
+    }
+    // eslint-disable-next-line
+    return user;
+  }
+}
