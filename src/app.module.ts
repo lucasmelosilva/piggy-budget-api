97130ec2e10af9578110b9cfc2765d8a971b8176
lucas melo <lucas.melo044@gmail.com>
2025-11-05 18:52:53 -0300

    chore: remove unncessary module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -23,7 +21,6 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
       logging: false,
       entities: [__dirname + '/**/*.entity{.js,.ts}'],
     }),
-    UsersModule,
     AuthModule,
   ],
   providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
