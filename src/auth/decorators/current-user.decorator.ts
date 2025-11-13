caca5dc633bc7caceb749d50e630489e3d054357
lucas melo <lucas.melo044@gmail.com>
2025-11-12 22:48:13 -0300

    feat: add CurrentUser decorator to retrieve authenticated user

---
+++ src/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { AuthRequest } from 'src/auth/model/AuthRequest';
+import { User } from 'src/users/user.entity';
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext): User => {
+    const request = context.switchToHttp().getRequest<AuthRequest>();
+    return request.user;
+  },
+);
